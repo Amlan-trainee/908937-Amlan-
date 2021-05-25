14d8a7819ea5727dc1e7fcb11dedcc5763b0ecb8
Amlan-Trainee <dasamlan2211@gmail.com>
2021-05-25 16:00:22 +0530

    .

--- JQuery/hands on/Q1/AddApplication/AddNumber.js
+++ JQuery/hands on/Q1/AddApplication/AddNumber.js
@@ -2,15 +2,15 @@ $(document).ready(function()
 {
     $('#btnAdd').on('click',function(){
         console.log("Hello");
-        var a=$("#firstname");
-        var b=$("#lastname");
+        var a=$("#firstnum");
+        var b=$("#lastnum");
         if(a.val()=='' || b.val()=='')
         {
             $('div').html("<div style='color:red;'> please, Enter the values </div>");
         }
         else{
-            var ans= parseInt(a.val())+parseInt(b.val());
-            $('div').html("<div style='color:green;'> answer </div>");
+            var ans = parseInt(a.val()) + parseInt(b.val());
+            $('div').html("<div style='color:green;'></div>").val(ans);
             $('#answer').val(ans);
         }
     });
