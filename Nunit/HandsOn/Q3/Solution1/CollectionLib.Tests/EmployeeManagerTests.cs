1325034921f81239a58cdfda07a64c2a3c4bc553
Amlan-Trainee <dasamlan2211@gmail.com>
2021-06-07 11:51:25 +0530

    Nunit

--- Nunit/HandsOn/Q3/Solution1/CollectionLib.Tests/EmployeeManagerTests.cs
+++ Nunit/HandsOn/Q3/Solution1/CollectionLib.Tests/EmployeeManagerTests.cs
@@ -13,22 +13,81 @@ namespace CollectionLib.Tests
     {
         List<Employee> employeelist;
         [Test]
-        public void TestCheckForNull()
+        public void NoNUllValueInCollectionTest()
         {
-            var emp = new EmployeeManager();
-            var emplist = emp.GetEmployees();
-            var expected= new List<Employee>
+            try
             {
-                new Employee { EmpId=100, EmpName="John",DOJ=DateTime.Now.AddYears(-5),Salary=30000},
-                new Employee { EmpId=101, EmpName="Mary",DOJ=DateTime.Now.AddYears(-2),Salary=10000},
-                new Employee { EmpId=102, EmpName="Steve",DOJ=DateTime.Now.AddYears(-2),Salary=10000},
-                new Employee { EmpId=103, EmpName="Allen",DOJ=DateTime.Now.AddYears(-7),Salary=50000},
-            };
-            CollectionAssert.AllItemsAreNotNull(emplist);
-            var empIdCheck = emplist.Where(n => n.EmpId == 100);
-            CollectionAssert.AreEqual(expected, emplist, new EmployeeComparer());
+                EmployeeManager em = new EmployeeManager();
+                employeelist = em.GetEmployees();
+                foreach (var data in employeelist)
+                {
+                    Assert.IsNotNull(data.EmpId);
+                    Assert.IsNotNull(data.EmpName);
+                    Assert.IsNotNull(data.DOJ);
+                    Assert.IsNotNull(data.Salary);
+                }
+            }
+            catch (Exception)
+            {
+                Assert.Fail("The data in the collection should not be null");
+            }
         }
+        [Test]
+        public void IDExistsTest()
+        {
+            int searchId = 100;
+            try
+            {
+                EmployeeManager em = new EmployeeManager();
+                employeelist = em.GetEmployees();
+                var findEmployee = employeelist.Where(x => x.EmpId == searchId).FirstOrDefault();
+                Assert.IsNotNull(findEmployee);
+
+            }
+            catch (Exception)
+            {
+                Assert.Fail("The searched id" + searchId + "is not found in the list");
+            }
+        }
+        [Test]
+        public void UniqueEmployee_Test()
+        {
+            try
+            {
+                EmployeeManager em = new EmployeeManager();
+                employeelist = em.GetEmployees();
+                for (int i = 0; i < employeelist.Count; i++)
+                {
+                    for (int j = i + 1; j < employeelist.Count; j++)
+                    {
+                        Assert.AreNotEqual(employeelist[i].EmpId, employeelist[j].EmpId);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                Assert.Fail("Test case is Failed as Id is not unique");
+            }
+        }
+
        
+       
+        [Test]
+        public void ListsSameTest()
+        {
+            try
+            {
+                EmployeeManager em = new EmployeeManager();
+                employeelist = em.GetEmployees();
+                List<Employee> employeelistbyDOJ = em.GetEmployeesWhoJoinedInPreviousYears();
+                CollectionAssert.AreEqual(employeelist, employeelistbyDOJ);
+            }
+            catch (Exception)
+            {
+                Assert.Fail("Both employeelists are not same");
+            }
+        }
+
 
     }
 }
